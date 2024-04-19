2ac32ba1806e0c650ef27401dcc1074faba404f4
Marcio Divonei <marcio230213@gmail.com>
2024-04-19 20:17:15 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-lojaebac/login.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress"/>
+
+describe('funcionalidade: Login', () => {
+
+    it('deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('marcio.teste@teste.com.br')
+        cy.get('#password').type('teste@123')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, marcio.teste')
+    })
+    
+})
