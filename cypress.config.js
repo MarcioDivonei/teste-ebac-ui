1c90d9a12d7a5204ddb3ab9e777dd7d491b8247c
Marcio Divonei <marcio230213@gmail.com>
2024-05-06 21:37:22 -0300

    mudanca no gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-
+  projectId: "ps3m1w",
  e2e: {
 
   baseUrl: "http://lojaebac.ebaconline.art.br/",
@@ -15,7 +15,7 @@ module.exports = defineConfig({
    
 
   },
-
+video: true,
  },
 
 });
